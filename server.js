4e897430d16b9de6fb647dae5682606cdfbd9f93
chanduimad <30651009+chanduimad@users.noreply.github.com>
2017-08-15 19:20:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,9 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/ui/test.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'test.html'));
+});
 
 app.get('/:articleName', function (req, res){
     var articleName = req.params.articleName;
