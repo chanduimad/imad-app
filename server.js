085bb9b9f2394fa227f9c2cb6cb7a91cda94f05b
chanduimad <30651009+chanduimad@users.noreply.github.com>
2017-09-03 17:41:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/:articleName', function (req, res){
 
 //db implementation
 app.get('/articles/:articleName', function (req, res){
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result){
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result){
         if(err){
             res.status(500).send(err.toString());
         } else {
